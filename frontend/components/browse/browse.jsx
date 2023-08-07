d821d12a3a896c185bbd1d9c77041812976a5912
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-08-07 17:10:57 +0500

    Browse All Page Done and Dusted

--- frontend/components/browse/browse.jsx
+++ frontend/components/browse/browse.jsx
@@ -5,9 +5,13 @@ import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
 import { fetchItems } from "../utils/fetchFunctions";
 import classes from "./browse.module.css";
+import { BiCartAdd } from "react-icons/bi";
+import Nav from "../ReusableComponents/nav";
+import { cinzel, roboto400 } from "../utils/fonts";
 
 export default function Browse() {
-  const { data, setData } = useContext(DataContext);
+  const { data, setData, isLoggedIn, loggedInEmail, setOrders } =
+    useContext(DataContext);
 
   useEffect(() => {
     fetchItems(setData);
@@ -32,26 +36,67 @@ export default function Browse() {
   // Ensure data is not null or undefined before filtering
   const filteredData =
     data?.filter((product) => !failedImageLinks.includes(product._id)) || [];
+
   return (
     <div className={classes.container}>
-      <div className={classes.browse}>
+      <Nav />
+      <div className={classes.item}>
         {filteredData.slice(0, visibleItems).map((product) => (
-          <Link
-            href={"/product/" + product._id}
-            className="link"
-            key={product._id}
-          >
-            <div>
-              <img
-                src={product.image_link}
-                alt="xyz"
-                onError={(e) => handleImageError(e, product._id)}
-              />
-              <h2>{product.name}</h2>
-              <h3>{product.brand}</h3>
-              <h3>{product.price + " " + product.currency}</h3>
-            </div>
-          </Link>
+          <div key={product._id}>
+            <img
+              src={product.image_link}
+              alt="xyz"
+              onError={(e) => handleImageError(e, product._id)}
+            />
+            <Link href={"/product/" + product._id} className="link">
+              <h2 className={cinzel.className}>
+                {product.name.length > 50
+                  ? `${product.name.slice(0, 50)}...`
+                  : product.name}
+              </h2>
+            </Link>
+            <h3 className={roboto400.className}>{product.price + " $"}</h3>
+            <h3 className={roboto400.className}>
+              {product.brand.toUpperCase()} {product.product_type.toUpperCase()}
+            </h3>
+            {isLoggedIn ? (
+              <section
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onClick={() => {
+                  setOrders((prevOrders) => ({
+                    email: loggedInEmail,
+                    orders: [
+                      ...prevOrders.orders,
+                      {
+                        id: product._id,
+                        name: product.name,
+                        price: product.price,
+                        timestamp: Date.now(),
+                      },
+                    ],
+                  }));
+                }}
+              >
+                <label className={roboto400.className}>
+                  ADD TO CART <BiCartAdd size={20} />
+                </label>
+              </section>
+            ) : (
+              <label className={roboto400.className}>
+                <Link
+                  href={"/login"}
+                  className="link"
+                  style={{ color: "black" }}
+                >
+                  LOGIN TO ADD TO CART
+                </Link>
+              </label>
+            )}
+          </div>
         ))}
       </div>
       {data && visibleItems < data.length && (
